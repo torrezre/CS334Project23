e9960648344aded1e5dafce4d3f98b8c762ea54f
torrezre <99754053+torrezre@users.noreply.github.com>
2023-04-26 14:51:10 -0600

    img filenames changed for better org
    some pwa implementation but doesn't work

--- Part 4/app.py
+++ Part 4/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail, Message
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myapp.db'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
