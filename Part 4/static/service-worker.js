e15b0f8d701be8c76cde4be4f628367440817de4
Zachary Marrs <51918310+zmarrs@users.noreply.github.com>
2023-04-29 18:05:58 -0600

    Added new service worker

--- Part 4/static/service-worker.js
+++ Part 4/static/service-worker.js
@@ -1,21 +1,75 @@
-// Install the service worker
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('creamcache').then(cache => {
-      //cache static files
-      return Promise.all([
-        cache.add('/'),
-        cache.add('{{ url_for('static', filename='fonts/EBGaramond-VariableFont_wght.ttf') }}'),
-        cache.add('{{ url_for('static', filename='fonts/JosefinSans-VariableFont_wght.ttf') }}'),
-        cache.add('{{ url_for('static', filename='images/creamery.png') }}'),
-        cache.add('{{ url_for('static', filename='images/menu-paletas') }}'),
-        cache.add('{{ url_for('static', filename='images/menu-bar') }}'),
-        cache.add('{{ url_for('static', filename='images/menu-gallons') }}'),
-        cache.add('{{ url_for('static', filename='images/menu-mochi') }}'),
-        cache.add('{{ url_for('static', filename='images/menu-sandwich') }}'),
-        cache.add('{{ url_for('static', filename='style.css') }}'),
-        cache.add('{{ url_for('static', filename='creamery.js') }}')
-      ]);
+const cacheName = 'flask-PWA';
+
+const filesToCache = [
+	'/static/fonts/EBGaramond-VariableFont_wght.ttf',
+	'/static/fonts/JosefinSans-VariableFont_wght.ttf',
+	'/static/style.css',
+	'/static/management.css',
+	'/static/images/creamery.png',
+	'/static/images/menu-bar.png',
+	'/static/images/menu-gallons.png',
+	'/static/images/menu-mochi.png',
+	'/static/images/menu-paletas.png',
+	'/static/images/menu-sandwich.png',
+	'/static/images/page-about-icecream.png',
+	'/static/images/page-contact-icecream.png',
+	'/static/images/product-bar-caramel nuts.png',
+	'/static/images/product-bar-caramel plain.png',
+	'/static/images/product-bar-chocolate nuts.png',
+	'/static/images/product-bar-chocolate plain.png',
+	'/static/images/product-gallon-chocolate.png',
+	'/static/images/product-gallon-cookies.png',
+	'/static/images/product-gallon-mango.png',
+	'/static/images/product-gallon-mint.png',
+	'/static/images/product-gallon-pecan.png',
+	'/static/images/product-gallon-rockyroad.png',
+	'/static/images/product-gallon-vanilla.png',
+	'/static/images/product-mochi-chocolate.png',
+	'/static/images/product-mochi-dulce.png',
+	'/static/images/product-mochi-greentea.png',
+	'/static/images/product-mochi-mango.png',
+	'/static/images/product-mochi-strawberry.png',
+	'/static/images/product-mochi-vanilla.png',
+	'/static/images/product-mochi-variety.png',
+	'/static/images/product-paleta-lime.png',
+	'/static/images/product-paleta-mango.png',
+	'/static/images/product-paleta-pineapple.png',
+	'/static/images/product-paleta-strawberry.png',
+	'/static/images/product-sandwich-chocolate.png',
+	'/static/images/product-sandwich-dblchocolate.png',
+	'/static/images/product-sandwich-vanilla.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
   );
 });
